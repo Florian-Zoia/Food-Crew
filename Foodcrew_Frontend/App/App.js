0500f930f3ef0a7b41f02044c1ec3a3652ba6ff8
Sv6n <sven.kux@com-software.de>
2022-02-18 17:59:54 +0100

    Moved app.js and structured pages

---
+++ Foodcrew_Frontend/App/App.js
@@ -0,0 +1,17 @@
+/**
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import Router from './Router/Router';
+
+export default class App extends Component {
+
+
+  render() {
+    return (
+            <Router />
+    );
+  }
+}
